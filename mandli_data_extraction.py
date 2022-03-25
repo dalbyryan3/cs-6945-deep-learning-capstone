62de6976ffa4f406573c690995b92e6be3b1aba9
dalbyryan3 <dalbyryan3@gmail.com>
2022-03-25 05:37:17 +0000

    Initial commit

---
+++ mandli_data_extraction.py
@@ -0,0 +1,18 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.13.7
+#   kernelspec:
+#     display_name: default
+#     language: python
+#     name: default
+# ---
+
+# %%
+import xml.etree.ElementTree as ET
+
---
+++ sync_notebooks_to_py.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+jupytext --set-formats ipynb,py:percent --sync 'mandli_data_extraction.ipynb'
